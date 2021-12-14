ea728d27712f5623113815da807719d68449173c
lokeshChinta <lokeech@gmail.com>
2021-12-14 11:56:04 +0530

    commiting working copy of Test

--- src/test/java/com/tests/FirstTest.java
+++ src/test/java/com/tests/FirstTest.java
@@ -2,15 +2,22 @@ package com.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
@@ -22,6 +29,9 @@ import io.testproject.sdk.drivers.web.EdgeDriver;
 public class FirstTest {
 	
 	public static ChromeDriver driver;
+	//public static EdgeDriver driver;
+	static File src=null;
+	static File dst=null;
 	
 	@BeforeAll
 	static void configure() throws MalformedURLException
@@ -45,22 +55,41 @@ public class FirstTest {
 	@Nested
 	class WhenNoSearchResultAreFound{
 	@Test
-	void shouldDisplayEmptySearchResult()
+	void shouldDisplayEmptySearchResult() throws IOException
 	{
+		System.out.println("Execution Started Successfully");
 		String expectedValue="Search: noresults";
 		driver.get("https://www.petrikainulainen.net/blog/");
+
+		takesScreenshot("HomePage");
+		FileUtils.copyFile(src, dst);
 		WebElement searchField=driver.findElement(By.id("s"));
 		searchField.sendKeys("noresults");
+		takesScreenshot("SearchPage");
+		FileUtils.copyFile(src, dst);
 		searchField.sendKeys(Keys.ENTER);
 		String actualValue=driver.findElement(By.xpath("//h1[@class='archive_title headline']")).getText();
+
+		takesScreenshot("ResultsPage");
+		FileUtils.copyFile(src, dst);
 		assertEquals(actualValue, expectedValue);
 	}
+	}
+	
+	
+	public static void takesScreenshot(String name)
+	{
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
+		LocalDateTime now = LocalDateTime.now();
+		src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		dst= new File(System.getProperty("user.dir")+"/Screenshots/"+name+dtf.format(now)+".jpg");
 	}
 	
 	@AfterAll
 	static void tearDown()
 	{
 		driver.quit();
+		System.out.println("Execution completed Successfully");
 	}
 
 }
